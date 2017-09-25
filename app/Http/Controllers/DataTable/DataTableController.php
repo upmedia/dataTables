3ddd9e5e2a57121ea9903b27a38b6daeb8c00226
Uffe Petersen <mail@upmedia.dk>
2017-09-25 22:32:55 +0200

    Initial commit

---
+++ app/Http/Controllers/DataTable/DataTableController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\DataTable;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Builder;
+
+abstract class DataTableController extends Controller
+{
+	protected $builder;
+
+    abstract public function builder();
+
+    public function __construct()
+    {
+    	$builder = $this->builder();
+
+
+    	if (!$builder instanceof Builder) {
+    		throw new \Exception('Not instance of Builder');
+    	}
+
+    	$this->builder = $builder;
+    }
+
+    public function index()
+    {
+    	return response()->json([
+    		'data' => [
+    			'table' => $this->builder->getModel()->getTable(),
+    			'displayable' => array_values($this->getDisplayableColumns()),
+    			'records' => $this->getRecords(),
+    		]
+    	]);
+
+    }
+
+    public function getDisplayableColumns()
+    {
+    	return array_diff($this->getDatabaseColumnNames(), $this->builder->getModel()->getHidden());
+    }
+
+    protected function getDatabaseColumnNames()
+    {
+    	return Schema::getColumnListing($this->builder->getModel()->getTable());
+    }
+
+    protected function getRecords()
+    {
+    	return  $this->builder->get($this->getDisplayableColumns());
+    }
+
+
+}
