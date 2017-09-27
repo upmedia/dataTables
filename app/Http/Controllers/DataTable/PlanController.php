9d9d0f264cd0666deae1d4629348e9de53d5d0f4
Uffe Petersen <mail@upmedia.dk>
2017-09-27 23:10:38 +0200

    Added another dataTable for testing

---
+++ app/Http/Controllers/DataTable/PlanController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\DataTable;
+
+use App\Plan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\DataTable\DataTableController;
+
+class PlanController extends DataTableController
+{
+    public function builder()
+    {
+    	return Plan::query();
+    }
+
+    public function getUpdatableColumns()
+    {
+        return [
+            'braintree_id', 'price', 'active', 'updated_at'
+        ];
+    }
+
+}
---
+++ app/Plan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $fillable = [
+    	'braintree_id', 'price', 'active'
+    ];
+}
